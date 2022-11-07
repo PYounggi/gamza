6a0f5ede33d6c62e70206828968bdc40560413d3
Young <hiyo2044@gmail.com>
2022-11-08 04:02:55 +0900

    .

--- Result.py
+++ Result.py
@@ -3,13 +3,29 @@ from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+import json
+from datetime import datetime
 
 class ResultScreen(QDialog):
     def __init__(self):
         super(ResultScreen, self).__init__()
         loadUi("ui/result.ui", self)
+
         self.img.setPixmap(QPixmap('recognize_img/test02.jpg').scaledToWidth(600).scaledToHeight(390))
         self.end.clicked.connect(self.goBack)
+
+        day = datetime.now().strftime("%Y-%m-%d")
+        with open('json_datas/' + day, 'r', encoding="utf-8") as read_file:
+            data = json.load(read_file)
+
+        num = 0
+        if int(datetime.now().strftime("%H")) >= 17 or int(datetime.now().strftime("%H")) <= 4:
+            for i in data['breakfast']:
+                num = num + 1
+
+            self.name.setText(str(data['breakfast'][num-1]['menu']))
+            self.kcal.setText(str(data['breakfast'][num-1]['kcal']))
+            self.gram.setText(str(data['breakfast'][num-1]['weight']))
 
     def goBack(self):
         self.deleteLater()
